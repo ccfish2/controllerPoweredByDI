1407a308bc7b1e44e2237c6207087323e088ed63
ccfish2 <jimin.susan.hu@gmail.com>
2025-04-24 20:55:20 -0700

    Fixing typo

--- pkg/model/translation/ingress/doc.go
+++ pkg/model/translation/ingress/doc.go
@@ -1,10 +1,8 @@
-
 package ingress
 
-import(
-	import (
-		"github.com/ccfish2/infra/pkg/logging"
-		"github.com/ccfish2/infra/pkg/logging/logfields"
-	)
+import (
+	"github.com/ccfish2/infra/pkg/logging"
+	"github.com/ccfish2/infra/pkg/logging/logfields"
 )
+
 var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "ingress-controller")
