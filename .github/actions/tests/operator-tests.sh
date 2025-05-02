15464d18668f5bfa4e1c2a52f8011eb9e536e16d
ccfish2 <jimin.susan.hu@gmail.com>
2025-05-02 11:04:35 -0700

    Automate pvc/pv creation for log controller

--- .github/actions/tests/operator-tests.sh
+++ .github/actions/tests/operator-tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # configure one microservice and its log through configmap
-# upload the log into S3 bucket
-# is there free S3 bucket resource available
+sudo mkdir -p /mnt/disks/logs/App1
+sudo touch  /mnt/disks/logs/App1/App1-20250501.log
+sudo chmod 777 /mnt/disks/logs/App1/App1-20250501.log
+echo "test daily log" >> /mnt/disks/logs/App1/App1-20250502.log
+
+# retrieve the log from S3 bucket and verify the content
