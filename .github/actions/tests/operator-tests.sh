1e54beb9fddee1c05940373b224c424ae3f7c299
ccfish2 <jimin.susan.hu@gmail.com>
2025-05-02 11:16:54 -0700

    Fix typo in deploy-img-ci.yaml

--- .github/actions/tests/operator-tests.sh
+++ .github/actions/tests/operator-tests.sh
@@ -2,6 +2,7 @@
 
 # configure one microservice and its log through configmap
 sudo mkdir -p /mnt/disks/logs/App1
+sudo chmod 777 /mnt/disks/logs/App1
 sudo touch  /mnt/disks/logs/App1/App1-20250501.log
 sudo chmod 777 /mnt/disks/logs/App1/App1-20250501.log
 echo "test daily log" >> /mnt/disks/logs/App1/App1-20250502.log
